5c1e9a7ee2b5e12c1edd879707362b3f1ca9d707
Aleksey <otomivetra@mail.ru>
2021-10-27 19:33:22 +0300

    task 3_2

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 require("./model/mongo/db");
-const User = require('./model/mongo/User');
-const Post = require('./model/mongo/Post');
-const Comment = require('./model/mongo/Comment');
+const UserController = require('./controller/User');
+const PostController = require('./controller/Post');
+const CommentController = require('./controller/Comment');
 const app = express();
 const PORT = 3001;
 
@@ -12,43 +12,14 @@ app.use(cors());
 app.use(express.static('public'))
 
 
-app.get('/users', async (req, res) => {
-  res.json({
-    items: await User.find()
-  });
-});
-app.get('/users/:id', async (req, res) => {
-  res.json({
-    item: await User.findOne({ id: +req.params.id })
-  })
-});
+app.get('/users', UserController.list);
+app.get('/users/:id', UserController.getById);
 
-app.get('/posts', async (req, res) => {
-  const userId = req.query.userId
-  // const {userId} = req.query
-  res.json({
-    items: await Post.find({ userId: +userId })
-  });
-});
+app.get('/posts', PostController.list);
+app.get('/posts/:id', PostController.getById);
 
-app.get('/posts/:id', async (req, res) => {
-  res.json({
-    item: await Post.findOne({ id: +req.params.id })
-  })
-});
-app.get('/comments', async (req, res) => {
-  const postId = req.query.postId
-
-  res.json({
-    items: await Comment.find({ postId: +postId })
-  });
-});
-
-app.get('/comments/:id', async (req, res) => {
-  res.json({
-    item: await Comment.findOne({ id: +req.params.id })
-  })
-});
+app.get('/comments', CommentController.list);
+app.get('/comments/:id', CommentController.getById);
 
 
 app.listen(PORT, () => {
